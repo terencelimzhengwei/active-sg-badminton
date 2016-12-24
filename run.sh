f8873b395bef226f04f65102d53f44c81bc81b9c
Terence Lim <terencelimz@gmail.com>
2016-12-24 21:08:58 +0800

    added latest.json

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 /home/chip/miniconda/bin/python /home/chip/active-sg-badminton/save_data.py
-/usr/bin/git add /home/chip/active-sg-badminton/data/latest.json
+cd /home/chip/active-sg-badminton
+/usr/bin/git add data/latest.json
 /usr/bin/git commit -m "adding latest.json"
 /usr/bin/git push origin master
+cd ~
